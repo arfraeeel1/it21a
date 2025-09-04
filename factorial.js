ebdd56b05f5c8bf2e3ea82c3adcf404d1cc46f90
arfraeeel1 <20241506@nbsc.edu.ph>
2025-09-04 08:27:45 +0800

    Added readline interface

--- factorial.js
+++ factorial.js
@@ -1,3 +1,10 @@
+const readline " require("readline");
+
+const rl = readline.createInterface({
+    input:ProcessingInstruction.stdin,
+    output:ProcessingInstruction.stdout
+})
+
 console.log("Factorial Application");
 
 console.log ("1. Say Hello");
